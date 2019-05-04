9b58c8d7ce53fa78ae7a5c8c04f8c98acb3b1658
splekhanov <sergeyplehanov2014@yandex.ru>
2019-05-04 10:44:51 +0300

    WAR packaging has been added

--- src/test/java/com/example/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/example/cruddemo/CruddemoApplicationTests.java
@@ -1,16 +1,19 @@
 package com.example.cruddemo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class CruddemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
